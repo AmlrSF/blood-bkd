a4c26869e09465ac76da8624d40bd70045305fbd
softyAmir <theyabcpssouaf2@gmail.com>
2024-04-29 09:31:54 -1100

    first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const connectToMongoDb = require('./dbConnection/connect');
+
+const customer = require('./routes/customersRoute');
+
+const cookieParser = require('cookie-parser');
+
+
+// middleawre
+app.use(cors());
+app.use(express.json({ limit: '50mb' }));
+app.use(cookieParser());
+
+
+//set route
+app.use('/api/v1/customers', customer);
+
+
+
+
+const runServerApplication = async () => {
+    try {
+        await connectToMongoDb(process.env.MONGO_URL_KEY);
+        app.listen(3000, () => {
+            console.log('the server is running on port 3000');
+        })
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+runServerApplication();
